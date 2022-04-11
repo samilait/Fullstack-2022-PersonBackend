df4dea05bf78aa244d077eb8317ae2e0eaae19b5
samilait <sami.p.laitinen@gmail.com>
2022-04-11 17:19:31 +0300

    heroku

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get('/info', (req, res) => {
     ${new Date()}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
 })
