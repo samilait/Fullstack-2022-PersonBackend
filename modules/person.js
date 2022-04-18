ed7b750fea980af802c583ce0d247041e1d3085d
samilait <sami.p.laitinen@gmail.com>
2022-04-18 12:30:21 +0300

    Tehtavat 3.19-3.21 tehty

--- modules/person.js
+++ modules/person.js
@@ -10,9 +10,32 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB: ', error.message)
   })
 
+  const numberValidator = (val) => {
+    const arr = val.split('-')
+    if (arr.length !== 2) {
+      return false
+    } else {
+      if (arr[0].length + arr[1].length < 8) {
+        return false
+      }
+      if (arr[0].length === 2 || arr.length[0].length === 3) {
+        return true
+      }
+    }
+
+  }
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minlength: 3
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: numberValidator,
+        message: 'Number must be at least 8 characters. Number must contain two parts separated with -. 1st part must be either 2 or 3 characters'
+        },
+    },    
     date: Date,
   })
     
